f957173f4ffb5f3e8cd066b4b875979eaaa5718e
Ervean <ethan@siminsights.com>
2024-08-08 19:00:40 -0700

    Added first data for double dashing

---
+++ niji-game/Assets/Data/Items/ItemPrefabs/Buffs/ItemBuffDashDoubleDash.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ervean.Utilities.Items;
+namespace Ervean.NijiGame.Items.Buffs
+{
+    public class ItemBuffDashDoubleDash : DroppedBuffItem
+    {
+       
+    }
+}
